d8be4537311e6eb18bbb4f2ea1b8e586b24990a6
Edson Rodrigues <thecodeverse12@gmail.com>
2024-07-07 23:07:00 +0100

    Player movement & game update complete!

--- main.py
+++ main.py
@@ -9,11 +9,23 @@ board = [
     ['-----------------']
 ]
 
-# posX = 0
-# posY = 0
+# Game variables
+player = "O"
+points = 0
+prevPlayerX = 0
+prevPlayerY = 0
 
+# Player positions
+playerX = 0
+playerY = 0
+
+
+# Draw the current state of the game board.
+# Prints the board with the current player position and points.
 def DrawBoard():
-    global board
+    global board, points, playerX, playerY
+
+    print(f"Points:{points}")
 
     for y in board:
         for x in y:
@@ -21,30 +33,58 @@ def DrawBoard():
     
     time.sleep(0.33)
 
-def ResetBoard():
-    global board
-
-    for row in board:
-        row[0] = row[0].replace("#", "-")
-
-
-def UpdateBoard():
-    global board
-
-    posY = random.randint(0, len(board) - 1)
-    posX = random.randint(0, len(board[posY][0]) - 1)
-
-    ResetBoard()
-    board[posY][0] = replaceString(board[posY][0], posX, "#")
-
+# Clear the console screen.
+def UpdateGame():
     os.system("cls||clear")
 
-def replaceString(string, pos, char):
-    string = list(string)
-    string[pos] = char
+# Update the player position on the game board.
+def UpdatePlayer():
+    global board, player, playerX, playerY, prevPlayerX, prevPlayerY
+    resetPreviousPosition()
+    prevPlayerX = playerX
+    prevPlayerY = playerY
+    # Updates the player
+    board[playerY][0] = updateObjects(board[playerY][0], playerX, player)
+    UpdateGame()
 
-    return ''.join(string)
+
+# Update a single object on the game board.
+def updateObjects(y, x, obj):
+    y = list(y)
+    y[x] = obj
+
+    return ''.join(y)
+
+# Waits for the user input to update de player position according to PlayerY & PlayerX
+def playerInput():
+    global board, playerX, playerY
+
+    key = input("Enter:")
+
+    if key.upper() == "W" and playerY > 0:
+        playerY -= 1
+        print("UP")
+
+    elif key.upper() == "D" and playerX < len(board[playerY][0]) - 1:
+        playerX += 1
+        print("RIGHT")
+
+    elif key.upper() == "A" and playerX > 0:
+        playerX -= 1
+        print("LEFT")
+
+    elif key.upper() == "S" and playerY < len(board) - 1:
+        playerY += 1
+        print("DOWN")
+    else:
+        pass
+
+# Resets the previous player position
+def resetPreviousPosition():
+    global board, prevPlayerX, prevPlayerY
+    board[prevPlayerY][0] = updateObjects(board[prevPlayerY][0], prevPlayerX, "-")
 
 while True:
+    UpdatePlayer()
     DrawBoard()
-    UpdateBoard()
+    playerInput()

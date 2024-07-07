b6e5a309b88ee8c93816db4c2f1f34250c3ad5ef
Edson Rodrigues <thecodeverse12@gmail.com>
2024-07-08 00:54:59 +0100

    Adding some dificulity

--- main.py
+++ main.py
@@ -2,23 +2,34 @@ import os
 import time
 import random
 
+# Game variables
+points = 0
 board = [
     ['-----------------'],
     ['-----------------'],
     ['-----------------'],
-    ['-----------------']
+    ['-----------------']   
 ]
-
-# Game variables
-player = "O"
-points = 0
-prevPlayerX = 0
-prevPlayerY = 0
-
-# Player positions
+# --- Player --- #
+player = "X"
+# --- Player positions --- #
 playerX = 0
 playerY = 0
+prevPlayerX = 0
+prevPlayerY = 0
+# --- Fruit --- #
+fruit = "O"
+# --- Fruit positions --- #
+fruitX = 0
+fruitY = 0
+prevFruitX = 0
+prevFruitY = 0
+# --- Obstacles --- #
+obstacle = "#"
+obstacleX = 0
+obstacleY = 0
 
+# --- GAME FUNCTIONS ---#
 
 # Draw the current state of the game board.
 # Prints the board with the current player position and points.
@@ -35,26 +46,68 @@ def DrawBoard():
 
 # Clear the console screen.
 def UpdateGame():
-    os.system("cls||clear")
-
-# Update the player position on the game board.
-def UpdatePlayer():
-    global board, player, playerX, playerY, prevPlayerX, prevPlayerY
-    resetPreviousPosition()
-    prevPlayerX = playerX
-    prevPlayerY = playerY
-    # Updates the player
-    board[playerY][0] = updateObjects(board[playerY][0], playerX, player)
-    UpdateGame()
+    UpdatePlayer()
 
 
 # Update a single object on the game board.
 def updateObjects(y, x, obj):
     y = list(y)
     y[x] = obj
-
     return ''.join(y)
 
+# --- PLAYER FUNCTIONS --- #
+# Update the player position on the game board.
+def UpdatePlayer():
+    global board, player, playerX, playerY, prevPlayerX, prevPlayerY
+    # Resets the previous player position
+    board[prevPlayerY][0] = updateObjects(board[prevPlayerY][0], prevPlayerX, "-")
+    prevPlayerX = playerX
+    prevPlayerY = playerY
+
+    # Updates the current player position
+    board[playerY][0] = updateObjects(board[playerY][0], playerX, player)
+    CheckFruitColision()
+    CheckObstacleColision()
+    os.system("cls||clear")
+
+
+# --- FRUITS FUNCTIONS --- #
+# Update the fruit position by random X & Y positions
+def UpdateFruit():
+    global board, fruit, fruitX, fruitY
+    # board[prevFruitY][0] = updateObjects(board[prevPlayerY][0], prevPlayerX, "-")
+    fruitY = random.randint(0, len(board) - 1)
+    fruitX = random.randint(0, len(board[fruitY][0]) - 1)
+
+    board[fruitY][0] = updateObjects(board[fruitY][0], fruitX, fruit)
+    os.system("cls||clear")
+
+def CheckFruitColision():
+    global playerX, playerY, fruitX, fruitY, points
+    if playerX == fruitX and playerY == fruitY:
+        points += 1 
+        UpdateObstacle() 
+        UpdateFruit()  # Updates fruit's position after colision
+
+# --- OBSTACLE FUNCTIONS --- #
+# Update the obstacle position by random X & Y positions
+def UpdateObstacle():
+    global board, obstacle, obstacleX, obstacleY
+
+    obstacleY = random.randint(0, len(board) - 1)
+    obstacleX = random.randint(0, len(board[obstacleY][0]) - 1)
+
+    board[obstacleY][0] = updateObjects(board[obstacleY][0], obstacleX, obstacle)
+    os.system("cls||clear")
+
+# Check if the player has collided with an obstacle
+def CheckObstacleColision():
+    global playerX, playerY, obstacleX, obstacleY
+    if playerX == obstacleX and playerY == obstacleY:
+        print("Game Over!")
+        exit()
+
+# --- USER FUNCTIONS --- #
 # Waits for the user input to update de player position according to PlayerY & PlayerX
 def playerInput():
     global board, playerX, playerY
@@ -79,12 +132,14 @@ def playerInput():
     else:
         pass
 
-# Resets the previous player position
-def resetPreviousPosition():
-    global board, prevPlayerX, prevPlayerY
-    board[prevPlayerY][0] = updateObjects(board[prevPlayerY][0], prevPlayerX, "-")
-
+# GAME RUNNER
+UpdateObstacle() # Initialize obstacle's position
+UpdateFruit() # Initialize fruit's position
 while True:
-    UpdatePlayer()
-    DrawBoard()
-    playerInput()
+    if points < 10:   
+        UpdateGame()
+        DrawBoard()
+        playerInput()
+    else:
+        print("You won!")
+        break
